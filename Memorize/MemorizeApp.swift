ade6ee27a2d448f7d8809048b919f6e4d84ced55
Andre Ricardo <andrericardoweb@gmail.com>
2022-10-20 00:39:41 -0300

    fisrt commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Andre Ricardo on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
